dde2bdd5359fbb21aa493fb61fbf75fea50293ad
Pablo <pablo.bollansee@gmail.com>
2020-05-21 22:56:53 +0100

    Trying out land/water compression, works well, 4 times bigger map, smaller outcome.

--- MapGenerator/ConvertMap_old.py
+++ MapGenerator/ConvertMap_old.py
@@ -1,6 +1,28 @@
 import os, sys
 from PIL import Image
 from tqdm import tqdm
+from helpers import *
+
+# Settings
+image_file = 'NE2_LR_LC_SR_W_DR.tif' #change me for different image
+output_file = 'map_compressed.lua' #change me for a different output file
+chunk_sizes = [32, 8]
+resize_width = None # Number (ex 500) or None
+
+# Globals
+Image.MAX_IMAGE_PIXELS = 1000000000 #large enough to allow huge map
+image_file = os.path.join(sys.path[0], image_file)
+image = Image.open(image_file).convert('RGB') # the image to use
+
+output_file = os.path.join(sys.path[0], output_file)
+output = open(output_file, 'w')
+
+# Optionally resize image
+if resize_width:
+    width, height = image.size
+    resize_height = int(float(height)*float(resize_width / float(width)))
+    print(f"Resizing image from {width}, {height} to {resize_width}, {resize_height}...")
+    image = image.resize((resize_width, resize_height), Image.ANTIALIAS)
 
 # Terrain codes
 terrain_codes = [
@@ -18,28 +40,6 @@ terrain_codes = [
     ((227,203,188), "S", "sand-3")
 ]
 
-#terrain_codes = [
-#    #((0,0,0), "_", "out-of-map"), #commented out those I don't want to generate
-#    ((220,220,254), "o", "deepwater"), #ocean
-#    #((24,39,14), "O", "deepwater-green"),
-#    ((246,241,254), "w", "water"),
-#    #((30,48,16), "W", "water-green"),
-#    ((102,184,47), "g", "grass-1"),
-#    ((141,187,59), "m", "grass-3"),
-#    ((175,209,69), "G", "grass-2"),
-#    ((209,194,61), "d", "dirt-3"),
-#    ((184,116,46), "D", "dirt-6"),
-#    #((241,237,209), "s", "sand-1"),
-#    #((227,203,188), "S", "sand-3")
-#]
-
-def color_color_distance(color1, color2):
-    r1, g1, b1 = color1
-    r2, g2, b2 = color2
-    #return (r1-r2) ** 2 + (g1-g2) ** 2 + (b1-b2) ** 2
-    rm = (r1+r2) / 2
-    return ((2+rm) * (r1-r2)) ** 2 + (4 * (g1-g2)) ** 2 + (3-rm * (b1-b2)) ** 2
-
 def get_terrain_letter(pixel):
     min_dist = float("inf")
     found_code = None
@@ -51,61 +51,60 @@ def get_terrain_letter(pixel):
     return found_code
 
 # Line converts
-def convert_line_full_text(im, y, width):
-    line = ""
-    for x in range(0, width):
-        pixel = im.getpixel((x, y))
-        code = get_terrain_letter(pixel)
-        line += code
-    return line
+# def convert_line_custom_compressed(im, y, width):
+#     line = ""
+#     current_letter = get_terrain_letter(im.getpixel((0, y)))
+#     current_count = 1
+#     for x in range(1, width):
+#         pixel = im.getpixel((x, y))
+#         letter = get_terrain_letter(pixel)
+#         if letter == current_letter:
+#             current_count += 1
+#         else:
+#             line += current_letter
+#             line += str(current_count)
+#             current_letter = letter
+#             current_count = 1
+#     line += current_letter
+#     line += str(current_count)
+#     return line
 
-def convert_line_custom_compressed(im, y, width):
-    line = ""
-    current_letter = get_terrain_letter(im.getpixel((0, y)))
-    current_count = 1
-    for x in range(1, width):
-        pixel = im.getpixel((x, y))
-        letter = get_terrain_letter(pixel)
-        if letter == current_letter:
+def convert_line_land_water(y, width):
+    line = []
+    currently_water = True # start with water always
+    current_count = 0
+    for x in range(0, width):
+        pixel = image.getpixel((x, y))
+        next_water = is_water(pixel)
+        if currently_water == next_water:
             current_count += 1
         else:
-            line += current_letter
-            line += str(current_count)
-            current_letter = letter
+            line.append(current_count)
+            currently_water = next_water
             current_count = 1
-    line += current_letter
-    line += str(current_count)
+    if current_count > 0:
+        line.append(current_count)
     return line
 
 # Writers
-def write_as_txt(lines, output_name):
-    output = open(output_name, 'w')
-    for line in lines:
-        output.write("%s\n" % line)
-
-def write_as_lua_array(lines, output_name):
-    output = open(output_name, 'w')
+def write_lua_land_water(lines):
     output.write("map_data = {\n")
     for line in lines[:-1]:
-        output.write("\t\"%s\",\n" % line)
-    output.write("\t\"%s\"\n" % lines[-1]) #last one without comma
+        output.write(f"\t{{{ ','.join(str(a) for a in line) }}},\n")
+    output.write(f"\t{{{ ','.join(str(a) for a in line) }}}\n") #last one without comma
     output.write("}")
 
 # Actual conversion code
-def convert(name, output_name, line_conversion_method = convert_line_custom_compressed, write_method = write_as_lua_array):
-    print("Converting: ", name)
-    Image.MAX_IMAGE_PIXELS = 1000000000 #large enough to allow huge map
-    im = Image.open(name).convert('RGB') # the image to use
-    print(im.format, im.size, im.mode)
-    width, height = im.size
+def convert_land_water():
+    print("Converting... ")
+    print(image.format, image.size, image.mode)
+    width, height = image.size
     lines = []
     for y in tqdm(range(0, height)):
-        line = line_conversion_method(im, y, width)
+        line = convert_line_land_water(y, width)
         lines.append(line)
 
-    write_method(lines, output_name)
+    write_lua_land_water(lines)
     print("Conversion done.")
 
-image_location = os.path.join(sys.path[0], 'NE2_LR_LC_SR_W_DR.tif') #change me for different image
-output_location = os.path.join(sys.path[0], 'map_compressed.lua') #change me for a different output file
-convert(image_location, output_location)
+convert_land_water()
